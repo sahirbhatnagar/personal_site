b3036d1b6fdf6d7d595d0c557ced0497853ac26f
Nick Strayer <nstrayer@uvm.edu>
2016-10-27 10:26:57 -0500

    added the new stuff

--- build_site.R
+++ build_site.R
@@ -4,5 +4,9 @@
 #working on at the time. 
 setwd("/Users/Nick/personal_site")
 
-#Render your sweet site. 
+#tender your sweet site. 
 rmarkdown::render_site()
+
+#this moves the rendered site back into the main level
+#of the repository. Ugly, but makes github easier.
+system("ditto _site/ .")
